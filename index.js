fff52ff18643607533d38e98b930c09522c93290
Krishna Teja <packetcode@gmail.com>
2020-08-15 16:00:41 +0530

    second commit

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get("/:id",(req,res)=>{
     //res.send("hello world");
     res.send(req.params);
 });
-console.log("hello world")
+console.log("hello world KT")
 
 app.listen(port, ()=>{ console.log("Listening on port 3000")})
