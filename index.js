e4e6afc0529da7e856eb69ee646cecb297e92705
Krishna Teja <packetcode@gmail.com>
2020-08-16 15:08:38 +0530

    sampl8

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import Express from "express";
 import Run from "./lib/run.js"
 import shelljs from "shelljs";
-import child_process from "child_process";
 
 
 
@@ -10,13 +9,16 @@ const port = 3000;
 
 
 app.get("/",(req,res)=>{
-
     res.send("Server to run coding - Krishna Teja ");
 });
 
 app.get("/code",(req,res)=>{
-    //res.send("hello world");
-    const str = Run();
+    var start = new Date()
+    var str = Run();
+    var end = new Date();
+    var time = end - start;
+    str.time = time;
+    console.info('Execution time: %dms', time)
     console.log(str.stdout);
     console.log(req.query.sample)
     res.send(str);
@@ -27,38 +29,7 @@ app.get("/hello",(req,res)=>{
     const str = shelljs.exec('echo "hello"');
     res.send(str);
 });
-app.get("/ls",(req,res)=>{
-    //res.send("hello world");
-    const str = shelljs.exec('ls');
-    const r = str.replace('\n','<br>');
-    res.send(r);
-});
 
-app.get("/whoami",(req,res)=>{
-    //res.send("hello world");
-    const str = shelljs.exec('whoami');
-    const r = str.replace('\n','<br>');
-    res.send(r);
-});
-
-app.get("/hello-world",(req,res)=>{
-    //res.send("hello world");
-    child_process.exec("docker run hello-world ", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            res.send(error.message);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            res.send(stderr);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        res.send(stdout);
-    });
-    
-});
 
 console.log("hello world KT")
 
