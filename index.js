906570f5a3a24bf6cd9ce2a777ad0d267956b625
Krishna Teja <packetcode@gmail.com>
2020-08-16 22:16:01 +0530

    sample3

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.post("/",(req,res)=>{
     var start = new Date()
     console.log(req.body)
     var payload = boot(req.body);
+    res.send(payload);
     console.log(payload)
     if(payload){
         var data = engine(req.body.lang,payload,req.body.name);
